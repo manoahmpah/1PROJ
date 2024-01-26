7106a28f52a490e8066f26fc5212a9df535ffd3d
Leon <mpahmanoah@gmail.com>
2024-01-26 17:02:57 +0100

    tableau

---
+++ pro.py
@@ -0,0 +1,22 @@
+
+def board():
+    board_2d = []
+    for (a, b, c) in [(6, 4, 1), (4, 7, 0), (3, 8, 0), (2, 9, 0), (1, 10, 0), (1, 9, 1), (0, 10, 1), (0, 9, 2),
+                      (0, 8, 3), (0, 7, 4), (1, 4, 6)]:
+        boardAnex = [9 for _ in range(a)] + [1 for _ in range(b)] + [9 for _ in range(c)]
+        board_2d.append(boardAnex)
+    return board_2d
+
+
+def Display(board, n):
+    board_2d = board
+    for row in range(n):
+        for col in range(n):
+            if board[row][col] == 9:
+                print(".", end=" ")
+            if board[row][col] == 1:
+                print("0", end=" ")
+        print("")
+
+
+Display(board(), 11)
