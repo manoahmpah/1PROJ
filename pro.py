396597f6b36ecbdd37db255255988c7a2aa0fcb6
Leon <mpahmanoah@gmail.com>
2024-02-09 11:38:25 +0100

    new feature

--- pro.py
+++ pro.py
@@ -3,6 +3,71 @@ class Pawn:
         self.__player = player
         self.__name = name
 
+        def isMoveValid(self, gameState, destination):
+        # Check that the destination coordinate is on the board and within the range
+        if not (0 <= destination[0] < numRows and 0 <= destination[1] < numCols):
+            return False
+
+        # Check that the destination coordinate is free
+        if not self.freeCoord(gameState.board, destination):
+            return False
+
+        # Check that the ring can be moved to the destination in all directions
+        for direction in directions:
+            newRow = destination[0] + direction[0]
+            newCol = destination[1] + direction[1]
+
+            # Check if the ring can move to the new coordinate
+            if self.ringMoves(gameState.board, gameState.activeRing, (newRow, newCol)):
+
+                # Check that the ring is owned by the current player
+                if gameState.turnMode != TurnMode.MoveRing:
+                    return False
+
+                return True
+
+        return False
+
+    def ringMoves(self, board, ringCoord, destinationCoord):
+        # Check that the ring coordinate is on the board
+        if not (0 <= ringCoord[0] < numRows and 0 <= ringCoord[1] < numCols):
+            return []
+
+        # Check that the ring is not already on an edge
+        if not (ringCoord[0] == 0 or ringCoord[1] == 0 or ringCoord[0] == numRows - 1 or ringCoord[1] == numCols - 1):
+            # Get the coordinates of the neighboring rings in each direction
+            neighbors = []
+            for direction in directions:
+                newRow = ringCoord[0] + direction[0]
+                newCol = ringCoord[1] + direction[1]
+
+                if newRow < 0 or newRow >= numRows or newCol < 0 or newCol >= numCols:
+                    continue
+
+                if self.freeCoord(board, (newRow, newCol)):
+                    neighbors.append((newRow, newCol))
+
+            # Add the neighbors as possible moves
+            moves = neighbors
+        else:
+            # If the ring is on an edge, it can only move orthogonally
+            moves = []
+            for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                newRow = ringCoord[0] + direction[0]
+                newCol = ringCoord[1] + direction[1]
+
+                if newRow < 0 or newRow >= numRows or newCol < 0 or newCol >= numCols:
+                    continue
+
+                if self.freeCoord(board, (newRow, newCol)):
+                    moves.append((newRow, newCol))
+
+        return moves
+
+    def freeCoord(board, coord):
+        row, col = coord
+        return board[row, col] is None
+
 
 class Logic:
     def __init__(self, name):
@@ -43,99 +108,6 @@ class Logic:
         else:
             print("Cette position est impossible !")
 
-    def OneDirection(self, PositionY, PositionX, i, j):
-
-        # Condition d'arrêt : la case est à l'intérieur du plateau et contient un pion adverse
-        return 1 + self.OneDirection(PositionY + i, PositionX + j, i, j) \
-            if (0 <= PositionX + j < self.__n
-                and 0 <= PositionY + i < self.__n
-                and self.__Board[PositionY + i][PositionX + j] == (-self.__PlayerToPlay)) \
-            else 0
-
-    # Méthode pour vérifier si un alignement de pions est présent dans toutes les directions depuis la position spécifiée
-    def AllDirection(self, PositionY, PositionX):
-        # Initialisation des compteurs pour chaque direction
-        column, line, Slash, = 0, 0, 0
-
-        # Parcours de toutes les directions
-        for index, (i, j) in enumerate([(0, -1), (0, 1), (-1, 0), (1, 0), (-1, 1), (1, -1)]):
-
-            # Comptage du nombre de pions adverses dans chaque direction
-            if index < 2:
-                column += self.OneDirection(PositionY, PositionX, i, j)
-            elif 2 <= index < 4:
-                line += self.OneDirection(PositionY, PositionX, i, j)
-            else:
-                Slash += self.OneDirection(PositionY, PositionX, i, j)
-
-        # Vérification s'il y a suffisamment de pions adverses dans au moins une direction
-        return True if column + 1 >= 5 or line + 1 >= 5 or Slash + 1 >= 5 else False
-
-
-    def isMoveValid(self, gameState, destination):
-        # Check that the destination coordinate is on the board and within the range
-        if not (0 <= destination[0] < self.__n and 0 <= destination[1] < self.__n):
-            return False
-
-        # Check that the destination coordinate is free
-        if not self.freeCoord(gameState.board, destination):
-            return False
-
-        # Check that the ring can be moved to the destination in all directions
-        for direction in directions:
-            newRow = destination[0] + direction[0]
-            newCol = destination[1] + direction[1]
-
-            # Check if the ring can move to the new coordinate
-            if self.ringMoves(gameState.board, gameState.activeRing, (newRow, newCol)):
-
-                # Check that the ring is owned by the current player
-                if gameState.turnMode != TurnMode.MoveRing:
-                    return False
-
-                return True
-
-        return False
-
-    def ringMoves(self, board, ringCoord, destinationCoord):
-        # Check that the ring coordinate is on the board
-        if not (0 <= ringCoord[0] < self.__n and 0 <= ringCoord[1] < self.__n):
-            return []
-
-        # Check that the ring is not already on an edge
-        if not (ringCoord[0] == 0 or ringCoord[1] == 0 or ringCoord[0] == self.__n - 1 or ringCoord[1] == self.__n - 1):
-            # Get the coordinates of the neighboring rings in each direction
-            neighbors = []
-            for direction in directions:
-                newRow = ringCoord[0] + direction[0]
-                newCol = ringCoord[1] + direction[1]
-
-                if newRow < 0 or newRow >= self.__n or newCol < 0 or newCol >= self.__n:
-                    continue
-
-                if self.freeCoord(board, (newRow, newCol)):
-                    neighbors.append((newRow, newCol))
-
-            # Add the neighbors as possible moves
-            moves = neighbors
-        else:
-            # If the ring is on an edge, it can only move orthogonally
-            moves = []
-            for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                newRow = ringCoord[0] + direction[0]
-                newCol = ringCoord[1] + direction[1]
-
-                if newRow < 0 or newRow >= self.__n or newCol < 0 or newCol >= self.__n:
-                    continue
-
-                if self.freeCoord(board, (newRow, newCol)):
-                    moves.append((newRow, newCol))
-
-        return moves
-
-    def freeCoord(board, coord):
-        row, col = coord
-        return board[row, col] is None
 
 logic_obj = Logic('Luc')
 # logic_obj.Display()
