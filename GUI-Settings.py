d1a8c5227bfb7d5fce5b4d02a62a670071c5bb5b
Manoah <mpahmanoah@gmail.com>
2024-02-14 15:25:50 +0100

    :recycle: in Class Mode

--- GUI-Settings.py
+++ GUI-Settings.py
@@ -1,41 +1,52 @@
 import pygame
 
-pygame.init()
 
-# ------- generer la fenetre de notre jeu ------- #
-pygame.display.set_caption("Settings")
-screen = pygame.display.set_mode((1080, 720))
+class GUISettings:
+    def __init__(self):
 
-backgroud = pygame.image.load("Assets-Setting/BackGround.png")
+        # ------- Initialize ------- #
+        self.__init = pygame.init()
+        self.__screen = pygame.display.set_mode((1080, 720))
+        pygame.display.set_caption("Settings")
 
-# ------- mettre du text ------- #
-font = pygame.font.Font(None, 36)
-Account = font.render("Account", True, (0, 0, 0))
-Languages = font.render("Languages", True, (0, 0, 0))
-Sound = font.render("Sound", True, (0, 0, 0))
-Customization = font.render("Customization", True, (0, 0, 0))
+        self.__running = True
 
-running = True
-# ------- boucle tant que cette conditiojn est vrai ------- #
-while running:
-    # appliquer l'arrière plan de notre Jeu
-    screen.blit(backgroud, (0, -200))
-    pygame.draw.rect(screen, (255, 255, 255), (0, 0, screen.get_width()/4, screen.get_height()))
+        # ------- Text ------- #
+        self.__font = pygame.font.Font(None, 36)
+        self.__Account = self.__font.render("Account", True, (0, 0, 0))
+        self.__Languages = self.__font.render("Languages", True, (0, 0, 0))
+        self.__Sound = self.__font.render("Sound", True, (0, 0, 0))
+        self.__Customization = self.__font.render("Customization", True, (0, 0, 0))
 
-    # affichage du texte
-    screen.blit(Account, (10, 15))
-    screen.blit(Languages, (10, 55))
-    screen.blit(Sound, (10, 95))
-    screen.blit(Customization, (10, 135))
+        # ------- BackGround ------- #
+        self.__BackGround = pygame.image.load("Assets-Setting/BackGround.png")
 
-    # mettre à jour notre ecran
-    pygame.display.flip()
+    def run(self):
+        while self.__running:
+            # ------- BackGround & Rectangle ------- #
+            self.__screen.blit(self.__BackGround, (0, -200))
+            pygame.draw.rect(self.__screen, (255, 255, 255),
+                             (0, 0, self.__screen.get_width() / 4, self.__screen.get_height()))
 
-    # si le joueur ferme cette fenetre
-    for event in pygame.event.get():
-        # Que l'evenement est fermeture de fenetre
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+            # pygame.draw.rect(self.__screen, (0, 0, 0), (self.__screen.get_width() / 4, 0, self.__screen.get_width()
+            # , self.__screen.get_height()))
+
+            # ------- Text ------- #
+            self.__screen.blit(self.__Account, (10, 15))
+            self.__screen.blit(self.__Languages, (10, 55))
+            self.__screen.blit(self.__Sound, (10, 95))
+            self.__screen.blit(self.__Customization, (10, 135))
+
+            # ------- MaJ ------- #
+            pygame.display.flip()
+
+            # ------- Close windows ------- #
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__running = False
+                    pygame.quit()
 
 
+if __name__ == "__main__":
+    settings = GUISettings()
+    settings.run()
