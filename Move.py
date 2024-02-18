72800c10260787d1f8682e9913461606662e684c
sher1ne <149390772+sher1ne@users.noreply.github.com>
2024-02-18 16:19:41 +0100

    Move

---
+++ Move.py
@@ -0,0 +1,71 @@
+def isMoveValid(self, gameState, i, j):
+    # Check that the destination coordinate is on the board and within the range
+    if not (0 <= i < self.__n and 0 <= j < self.__n):
+        return False
+
+    # Check that the destination coordinate is free
+    if not self.freeCoord(self.__Board, (i, j)):
+        return False
+
+    # Define directions here
+    directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, 1), (1, -1)]
+
+    # Check that the ring can be moved to the destination in all directions
+    for direction in directions:
+        newRow = i + direction[0]
+        newCol = j + direction[1]
+
+        # Check if the ring can move to the new coordinate
+        if self.ringMoves(self.__Board, gameState.activeRing, (newRow, newCol)):
+
+            # Check that the ring is owned by the current player
+            if gameState.turnMode != TurnMode.MoveRing:
+                return False
+
+            return True
+
+    return False
+
+
+
+def ringMoves(board, ringCoord):
+    # Check that the ring coordinate is on the board
+    # numCols & numRow while be remplace by self.__n
+    if not (0 <= ringCoord[0] < numRows and 0 <= ringCoord[1] < numCols):
+        return []
+
+    # Check that the ring is not already on an edge
+    if not (ringCoord[0] == 0 or ringCoord[1] == 0 or ringCoord[0] == numRows - 1 or ringCoord[1] == numCols - 1):
+        # Get the coordinates of the neighboring rings in each direction
+        neighbors = []
+        for direction in directions:
+            newRow = ringCoord[0] + direction[0]
+            newCol = ringCoord[1] + direction[1]
+
+            if newRow < 0 or newRow >= numRows or newCol < 0 or newCol >= numCols:
+                continue
+
+            if freeCoord(board, (newRow, newCol)):
+                neighbors.append((newRow, newCol))
+
+        # Add the neighbors as possible moves
+        moves = neighbors
+    else:
+        # If the ring is on an edge, it can only move orthogonally
+        moves = []
+        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            newRow = ringCoord[0] + direction[0]
+            newCol = ringCoord[1] + direction[1]
+
+            if newRow < 0 or newRow >= numRows or newCol < 0 or newCol >= numCols:
+                continue
+
+            if freeCoord(board, (newRow, newCol)):
+                moves.append((newRow, newCol))
+
+    return moves
+
+
+def freeCoord(board, coord):
+    row, col = coord
+    return board[row, col] is None
