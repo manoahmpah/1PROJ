e94a4f361a5c7ed82041637dade8400e348bd131
Manoah <88939578+manoahmpah@users.noreply.github.com>
2024-05-17 11:03:21 +0200

    :construction_worker: split in many class

--- GameBoard/EventHandler.py
+++ GameBoard/EventHandler.py
@@ -1,15 +1,18 @@
 import pygame
+from GameLogicHandler import GameLogicHandler
 
-class EventHandler:
+
+class EventHandler(GameLogicHandler):
     def __init__(self, game_logic_handler, board_renderer):
+        super().__init__(game_logic_handler, board_renderer)
         self.game_logic_handler = game_logic_handler
         self.board_renderer = board_renderer
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.game_logic_handler.logic_obj.get_pawn_number_on_board() < 10:
-                self.game_logic_handler.put_on_click()
-                self.game_logic_handler.reinitialise_click()
+            if self.game_logic_handler.get_pawn_number_on_board() < 10:
+                self._put_on_click()
+                self._reinitialise_click()
             else:
                 self.game_logic_handler.move_pawns()
 
